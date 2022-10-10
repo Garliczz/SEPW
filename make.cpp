d53ebef57aedc93d7a6e9f3d003dd58861af22c6
garliczz <1214156779@qq.com>
2022-10-10 18:21:03 +0800

    solution&data&make

---
+++ make.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //生成班级名单
+	system("data.exe > data.txt");
+    //生成抽点名单并计算E
+	system("solution.exe < data.txt > solution.txt");
+	return 0;
+}
